37067a73f707497fa0783f5d340d71228912f7be
maelaubert56 <maelaubert56@gmail.com>
2023-09-20 22:39:20 +0800

    page account

--- front/src/pages/Account/Account.js
+++ front/src/pages/Account/Account.js
@@ -17,9 +17,10 @@ function Account() {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const navigate = useNavigate();
 
-    useEffect(() => {
+
+    /*useEffect(() => {
         if(sessionStorage.getItem("session")) navigate("/");//TODO: redirect to account
-    }, [logged])
+    }, [logged])*/
 
     const createSession = (user) => {
         sessionStorage.setItem("session", JSON.stringify(user))
@@ -50,8 +51,27 @@ function Account() {
         })
     }
 
-
     return (
+        {logged} ?
+        <div className='account_page'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none" className='backarrow' onClick={() => window.location.href = '/'}>
+                <path d="M0.939339 10.9393C0.353554 11.5251 0.353554 12.4749 0.939339 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97918 12.6066 1.3934C12.0208 0.80761 11.0711 0.80761 10.4853 1.3934L0.939339 10.9393ZM33 10.5L2 10.5L2 13.5L33 13.5L33 10.5Z"/>
+            </svg>
+            <img src={logo} alt="logo" className='logo'/>
+            <form onSubmit={handleSubmit(onSubmitRegister)} className='form_register'>
+                <div className="pp_list">
+                    <img src={`/profilePictures/pp0.png`} alt="Profile Pic" className="ppSelected"/>
+                </div>
+                <input type="text" {...register("username")} placeholder="Nom d'utilisateur"/>
+                <input type="text" {...register("firstName")} placeholder="Prénom"/>
+                <input type="text" {...register("lastName")} placeholder="Nom"/>
+                <input type="password" {...register("password")} placeholder="Mot de passe"/>
+
+                <input type="submit" value="Appliquer" />
+                <p onClick={() => console.log("deconnexion")} className='deconnexion'>Se déconnecter</p>
+            </form>
+        </div>
+        :
         <div className='account_page'>
             <svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none" className='backarrow' onClick={() => window.location.href = '/'}>
                 <path d="M0.939339 10.9393C0.353554 11.5251 0.353554 12.4749 0.939339 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97918 12.6066 1.3934C12.0208 0.80761 11.0711 0.80761 10.4853 1.3934L0.939339 10.9393ZM33 10.5L2 10.5L2 13.5L33 13.5L33 10.5Z"/>
