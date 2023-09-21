e030f3eea2bd4f9f50a43a028610e54523976e42
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2023-09-21 10:51:40 +0800

    redirect to account when not logged in

--- front/src/pages/MenuPartie/MenuPartie.js
+++ front/src/pages/MenuPartie/MenuPartie.js
@@ -9,12 +9,17 @@ import { format } from 'date-fns';
 function MenuPartie() {
     const [isPopupDisplayed, setPopupDiplayed] = useState(false)
     const [parties, setParties] = useState([])
+    const [session, setSession] = useState(sessionStorage.getItem("session"))
+
 
     useEffect(() => {
-        const {username} = JSON.parse(sessionStorage.getItem("session"))
-        axios.get(`${process.env.REACT_APP_API}/users/${username}`).then(res => {
-            setParties(res.data.playerInGames)
-        })
+        if(session)
+            axios.get(`${process.env.REACT_APP_API}/users/${session.username}`).then(res => {
+                setParties(res.data.playerInGames)
+            })
+        else{
+            window.location.href = "/account"
+        }
     }, [isPopupDisplayed])
 
 
@@ -30,11 +35,11 @@ function MenuPartie() {
 
     return (
 
-        <div className='cards-area'>
+        <div className='cards-area'> 
             <svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none" className='backarrow' onClick={() => window.location.href = '/'}>
                 <path d="M0.939339 10.9393C0.353554 11.5251 0.353554 12.4749 0.939339 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97918 12.6066 1.3934C12.0208 0.80761 11.0711 0.80761 10.4853 1.3934L0.939339 10.9393ZM33 10.5L2 10.5L2 13.5L33 13.5L33 10.5Z"/>
             </svg>
-            {parties.length > 0 && parties.map((partie, index) => (
+            {session && parties.length > 0 && parties.map((partie, index) => (
                 <Card
                     index={index}
                     id={partie.idPartie}
