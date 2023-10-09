04a00739e09358f78cb1ed439bae9c602d3b9205
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2023-10-09 13:16:28 +0800

    test

--- front/src/pages/Account/Account.js
+++ front/src/pages/Account/Account.js
@@ -35,11 +35,7 @@ function Account() {
 
     const onSubmitLogin = async (data) => {
         const {username, password} = data
-        axios.get(process.env.REACT_APP_API + "/users/matt").then(res => {
-            console.log(res)
-        }).catch(e => {
-            console.log(e)
-        })
+        console.log(`${process.env.REACT_APP_API}/users/${username}`)
         const user = await axios.get(`${process.env.REACT_APP_API}/users/${username}`)
         if(user.data){
             if(bcrypt.compareSync(password, user.data.password)) createSession(user.data)
