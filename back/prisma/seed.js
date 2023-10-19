26fb7239691230a823639b7094413b2a98bc558e
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2023-10-19 18:45:39 +0800

    seed bugfix

--- back/prisma/seed.js
+++ back/prisma/seed.js
@@ -4,14 +4,22 @@ const bcrypt = require('bcryptjs')
 const salt = bcrypt.genSaltSync(10)
 
 const add = async () => {
-    await prisma.users.create({
-        data: {
-            username: "superadmin",
-            avatar: 957153,
-            password: bcrypt.hashSync("DZvz&kFo23X9Z&iiHF*Z", salt),
-            privilege: 2
+
+    const superadmin = await prisma.users.findUnique({
+        where: {
+            username: "superadmin"
         }
     })
+
+    if(!superadmin)
+        await prisma.users.create({
+            data: {
+                username: "superadmin",
+                avatar: 957153,
+                password: bcrypt.hashSync("DZvz&kFo23X9Z&iiHF*Z", salt),
+                privilege: 2
+            }
+        })
 }
 
 add()
