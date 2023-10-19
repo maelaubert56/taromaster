718fa7486aa6a6eea2ccdf0de69ebb08bc606d00
Mattéo Bonnet <49744127+matt91320@users.noreply.github.com>
2023-10-19 19:06:12 +0800

    cors

--- back/app.js
+++ back/app.js
@@ -3,26 +3,17 @@ const app = express()
 const port = 3333
 const cors = require('cors');
 
+app.use(cors({
+  origin: 'https://taromaster.matteobonnet.fr'
+}));
+
 const userRoutes = require('./routes/Users')
 const partiesRoutes = require("./routes/Parties")
 const donnesRoutes = require('./routes/Donnes')
 const playerInGamesRoutes = require("./routes/PlayerInGame")
 const adminRoutes = require("./routes/Admin")
 
-const corsOpts = {
-  origin: 'https://taromaster.matteobonnet.fr',
 
-  methods: [
-    'GET',
-    'POST',
-  ],
-
-  allowedHeaders: [
-    'Content-Type',
-  ],
-};
-
-app.use(cors(corsOpts));
 app.use(express.json({limit: '50mb'}));
 
 app.use('/users', userRoutes)
